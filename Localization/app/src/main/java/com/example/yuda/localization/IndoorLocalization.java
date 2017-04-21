313201cb5657969250624da9f46fb2c010d2431f
yuudaa <v420746@gmail.com>
2017-04-21 22:08:21 +0800

    reCreate
    Botton broken always on API16,
    so I changed API level to API17.

---
+++ Localization/app/src/main/java/com/example/yuda/localization/IndoorLocalization.java
@@ -0,0 +1,13 @@
+package com.example.yuda.localization;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndoorLocalization extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indoor_localization);
+    }
+}
