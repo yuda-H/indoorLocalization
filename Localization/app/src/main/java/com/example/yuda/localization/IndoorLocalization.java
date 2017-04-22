79c6b174ddd4541fa290f31331aa56818aec78c5
yuudaa <v420746@gmail.com>
2017-04-22 15:26:17 +0800

    scan WIFI
    scan WIFI per second

--- Localization/app/src/main/java/com/example/yuda/localization/IndoorLocalization.java
+++ Localization/app/src/main/java/com/example/yuda/localization/IndoorLocalization.java
@@ -1,18 +1,31 @@
 package com.example.yuda.localization;
 
 import android.content.Context;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TabHost;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class IndoorLocalization extends AppCompatActivity {
     TabHost tabHost;
     TabHost.TabSpec mTabSpec;
     WifiManager mWifiManager;
     TextView txt_timer,scanOrNot;
+    Handler handler;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Stop thread when you leave this app
+        handler.removeCallbacks(runnable);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +33,7 @@ public class IndoorLocalization extends AppCompatActivity {
 
         setContentView(R.layout.activity_indoor_localization);
         setTabHost();
+        handler = new Handler();
     }
 
 
@@ -39,13 +53,40 @@ public class IndoorLocalization extends AppCompatActivity {
 
     }
 
+    final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            String sss = "";
+            mWifiManager.startScan();
+            List<ScanResult> resultList = mWifiManager.getScanResults();
+            try {
+                for (int i = 0; i < resultList.size(); i++) {
+                    ScanResult result = resultList.get(i);
+                    sss +=  "\n" + result.SSID + "\n" + result.BSSID + "\b\b\b" + result.level + "\n";
+                }
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+            txt_timer.setText(sss);
+            handler.postDelayed(this, 1000);
+        }
+    };
+
     public void scan_start(View view) {
+        txt_timer = (TextView)findViewById(R.id.txt_tab1);
         scanOrNot = (TextView)findViewById(R.id.txt_tab2);
         scanOrNot.setText("你已經開始掃描了");
+        handler.post(runnable);
+
+
+
     }
 
     public void scan_stop(View view) {
+        txt_timer = (TextView)findViewById(R.id.txt_tab1);
         scanOrNot = (TextView)findViewById(R.id.txt_tab2);
         scanOrNot.setText("你已經結束掃描了");
+        handler.removeCallbacks(runnable);
+
     }
 }
